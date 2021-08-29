3539965da76c1a17d038806d25eee37ab789695e
Alexander Germek <aggermek@mail.ru>
2021-08-29 22:32:08 +0300

    fix some code

--- Devices/Other/Extensions.swift
+++ Devices/Other/Extensions.swift
@@ -65,9 +65,9 @@ extension UIView {
             
             let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation")
 
-            rotationAnimation.fromValue = 0.0
-            rotationAnimation.toValue = Float.pi * 2.0
-            rotationAnimation.duration = duration
+            rotationAnimation.fromValue   = 0.0
+            rotationAnimation.toValue     = Float.pi * 2.0
+            rotationAnimation.duration    = duration
             rotationAnimation.repeatCount = Float.infinity
 
             layer.add(rotationAnimation, forKey: UIView.kRotationAnimationKey)
